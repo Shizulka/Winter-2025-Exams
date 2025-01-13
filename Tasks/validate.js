8f47367185001e46c64548743881269d72e2e5b6
Shizulka <yrina.lishuk2006@gmail.com>
2025-01-13 23:21:26 +0200

    cycle simplification

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -4,18 +4,10 @@
 const isValidate = (T) => {
   if (typeof T !== 'string' || T.length === 0 || !T.includes(' ')) return false;
 
-    for (C of T) {
-      if (C === ' ') continue;
-      if (
-        C.toLowerCase().charCodeAt(0) >= 97 &&
-        C.toLowerCase().charCodeAt(0) <= 122
-      ) {
-      } else {
-        return false;
-      }
-    }
-    return true;
-
-};
+  for (const C of T) {
+    if (C !== ' ' && !/^[a-zA-Z]$/.test(C)) return false;
+  }
+  return true;
+}
 
 module.exports = isValidate;
