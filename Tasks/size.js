11dfbc1dbef8b193751f9f20723e991f72acf5d6
Shizulka <yrina.lishuk2006@gmail.com>
2025-01-13 20:27:47 +0200

    let to const

--- Tasks/size.js
+++ Tasks/size.js
@@ -1,7 +1,7 @@
 // Refactor following solution
 // Convert number to file size in bytes, kb, mb, and gb
 
-let size = (size) => {
+const size = (size) => {
   if (size === 0) return '0 byte';
   else {
     const exp = Math.floor(Math.log(size) / Math.log(1000));
