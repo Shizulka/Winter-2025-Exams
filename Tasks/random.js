e9ebc36673e425a2a44d32266167abaedc18f242
Shizulka <yrina.lishuk2006@gmail.com>
2025-01-13 18:48:42 +0200

    remove  return Math.floor(Math.random() * (max + 1));

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,7 +1,6 @@
 const Random = (min, max = min ) => {
     max === undefined;
 
-    return Math.floor(Math.random() * (max + 1));
     return min + Math.floor(Math.random() * (max - min + 1));
 };
 
