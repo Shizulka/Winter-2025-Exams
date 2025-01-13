0ba92975d1105a2472c7d2540891e88c8d3e400d
Shizulka <yrina.lishuk2006@gmail.com>
2025-01-13 18:45:55 +0200

    move max = min

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,6 +1,6 @@
-Random = (min, max) => {
+const Random = (min, max = min ) => {
     max === undefined;
-    max = min;
+
     return Math.floor(Math.random() * (max + 1));
     return min + Math.floor(Math.random() * (max - min + 1));
 };
