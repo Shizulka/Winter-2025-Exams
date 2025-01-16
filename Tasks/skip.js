b7c251239d15f85012b15485c4acb9849eec7f7d
Shizulka <yrina.lishuk2006@gmail.com>
2025-01-16 11:21:48 +0200

    used  filter

--- Tasks/skip.js
+++ Tasks/skip.js
@@ -1,17 +1,4 @@
-// Refactor following solution
-// Return an remove without listed values
 
-Skip = (T, ...Remove) => {
-  x = 0;
-  for (C of T) {
-    for (X of Remove) {
-      if (C === X) {
-        T.splice(x, 1);
-      }
-    }
-    x++;
-  }
-  return T;
-};
+Skip = (T, ...Remove) => T.filter(item => !Remove.includes(item));
 
 module.exports = Skip;
